47ff052602746eca2ee04122fee4e41cce71dc74
Jonathan Riddell <jr@jriddell.org>
2018-03-19 18:21:47 +0000

    sync list view to main site

--- list/index.php
+++ list/index.php
@@ -55,72 +55,122 @@ print "<p>Most recent SEPA reading: " . $riverSections->calculateMostRecentReadi
 &nbsp;<img src='/wheres-the-water/pics/ukrgb.ico'/> Online Guidebook Link<br />
 &nbsp;<img title='Access Issue Link' src='/wheres-the-water/pics/warning.png' /> Access Issue Links<br />
 </p>
-<table id="riverTable">
-  <tr>
-   <!--When a header is clicked, run the sortTable function, with a parameter, 0 for sorting by names, 1 for sorting by country:-->  
-    <th onclick="sortTable(0)">Name</th>
-    <th onclick="sortTable(1)">Level</th>
-    <th onclick="sortTable(2)">Grade</th>
-    <th>Links</th>
-  </tr>
-  <?php $riverSections->printTable(); ?>
-</table>
+        	
+        	<p>Search by river name: <input type="text" name="table-search" id="table-search"/></p>
+        	<p>Click on River Section, Grade or Level to sort the table</p>
+        	
+        	<table id="river-table">
+        		<tr>
+        			<th class='clickable' id='js-river-name'>River Section <span class='order-arrow'>&#x25BC;</span></th>
+        			<th class='clickable' id='js-river-grade'>Grade <span class='order-arrow'></span></th>
+        			<th class='clickable' id='js-river-level'>Level <span class='order-arrow'></span></th>
+        			<th>Trend</th>
+        			<th>Link</th>
+        		</tr>
+        		<?php $riverSections->printTable();?>
+        	</table>
 
 <script>
-function sortTable(n) {
-  var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
-  table = document.getElementById("riverTable");
-  switching = true;
-  //Set the sorting direction to ascending:
-  dir = "asc"; 
-  /*Make a loop that will continue until
-  no switching has been done:*/
-  while (switching) {
-    //start by saying: no switching is done:
-    switching = false;
-    rows = table.getElementsByTagName("TR");
-    /*Loop through all table rows (except the
-    first, which contains table headers):*/
-    for (i = 1; i < (rows.length - 1); i++) {
-      //start by saying there should be no switching:
-      shouldSwitch = false;
-      /*Get the two elements you want to compare,
-      one from current row and one from the next:*/
-      x = rows[i].getElementsByTagName("TD")[n];
-      y = rows[i + 1].getElementsByTagName("TD")[n];
-      /*check if the two rows should switch place,
-      based on the direction, asc or desc:*/
-      if (dir == "asc") {
-        if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
-          //if so, mark as a switch and break the loop:
-          shouldSwitch= true;
-          break;
-        }
-      } else if (dir == "desc") {
-        if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
-          //if so, mark as a switch and break the loop:
-          shouldSwitch= true;
-          break;
-        }
-      }
-    }
-    if (shouldSwitch) {
-      /*If a switch has been marked, make the switch
-      and mark that a switch has been done:*/
-      rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
-      switching = true;
-      //Each time a switch is done, increase this count by 1:
-      switchcount ++;      
-    } else {
-      /*If no switching has been done AND the direction is "asc",
-      set the direction to "desc" and run the while loop again.*/
-      if (switchcount == 0 && dir == "asc") {
-        dir = "desc";
-        switching = true;
-      }
-    }
-  }
-}
+// ----------------- Table sorting -----------------------------
+jQuery(document).ready( function(){
+
+	var downArrow = "&#x25BC;";
+	var upArrow = "&#x25B2;";
+	// Initial order, alphabetical by river name
+	sortTable("river-table", "riverSectionRow", 0, true);
+
+	jQuery('#js-river-name').on('click', function(){
+		jQuery('.order-arrow').html('');
+		if (jQuery(this).hasClass('sort-asc')){
+			sortTable("river-table", "riverSectionRow", 0, false);
+			jQuery(this).removeClass('sort-asc');
+    		jQuery(this).find('.order-arrow').html(upArrow);
+		}
+		else {
+    		sortTable("river-table", "riverSectionRow", 0, true);
+    		jQuery(this).find('.order-arrow').html(downArrow);
+    		jQuery(this).addClass('sort-asc');
+		}
+	});
+	jQuery('#js-river-grade').on('click', function(){
+		jQuery('.order-arrow').html('');
+		if (jQuery(this).hasClass('sort-asc')){
+			sortTable("river-table", "riverSectionRow", 1, false);
+			jQuery(this).removeClass('sort-asc');
+			jQuery(this).find('.order-arrow').html(upArrow);
+		}
+		else {
+			sortTable("river-table", "riverSectionRow", 1, true);
+			jQuery(this).addClass('sort-asc');
+			jQuery(this).find('.order-arrow').html(downArrow);
+		}
+	});
+	jQuery('#js-river-level').on('click', function(){
+		jQuery('.order-arrow').html('');
+		if (jQuery(this).hasClass('sort-asc')){
+			sortTable("river-table", "riverSectionRow", 4, false);
+			jQuery(this).removeClass('sort-asc');
+			jQuery(this).find('.order-arrow').html(upArrow);
+			
+		}
+		else {
+			sortTable("river-table", "riverSectionRow", 4, true);
+			jQuery(this).addClass('sort-asc');
+			jQuery(this).find('.order-arrow').html(downArrow);
+		}
+	});
+});
+
+sortTable = function(tableName, rowClass, columnNumber, ascending) {
+    var row, cell, cellContent;
+    var comparisonRow, comparisonCell, comparisonContent;
+
+    $("#" + tableName + " tr." + rowClass).each(function(i) {
+        row = $("#" + tableName + " tr." + rowClass + ":eq(" + i + ")");
+        cell = $(row).find("td:eq(" + columnNumber + ")");
+        cellContent = $(cell).html();
+
+        $("#" + tableName + " tr." + rowClass).each(function(j) {
+            comparisonRow = $("#" + tableName + " tr." + rowClass + ":eq(" + j + ")");
+            comparisonCell = $(comparisonRow).find("td:eq(" + columnNumber + ")");
+            comparisonContent = $(comparisonCell).html();
+
+            if ( (ascending && cellContent < comparisonContent) || (!ascending && cellContent > comparisonContent) ) {
+                $(row).insertBefore(comparisonRow);
+                return false;
+            }
+        });
+    });
+};
+
+</script>
+<script>
+// ------------------ Table search ---------------------------------------------
+jQuery(document).ready( function(){
+	jQuery('#table-search').on('keyup', function(){
+		var searchString = jQuery(this).val().toLowerCase();
+		if (searchString != ''){
+			jQuery('.riverSectionRow').each( function(){
+				if (jQuery(this).find('.riverSection').text().toLowerCase().indexOf(searchString) >= 0){
+					jQuery(this).show();
+				}
+				else {
+					jQuery(this).hide();
+				}
+				if (searchString == 'nesk' && jQuery(this).find('.riverSection').text().toLowerCase().indexOf('north esk') >= 0){
+					jQuery(this).show();
+				}
+				if (searchString == 'the best river' && jQuery(this).find('.riverSection').text().toLowerCase().indexOf('tilt') >= 0){
+					jQuery(this).show();
+				}
+			});
+				
+		}
+		else {
+			jQuery('.riverSectionRow').show();
+		}
+	});
+});
 </script>
 
 <?php
