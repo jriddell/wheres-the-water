af708570676524844044bcae4868d4519c37c8e6
Jonathan Riddell <jr@jriddell.org>
2018-05-13 15:53:49 +0000

    convert map to leaflet/osm

--- common.php
+++ common.php
@@ -4,6 +4,7 @@ function heading() {
 ?>
 <!DOCTYPE html>
 <head>
+    <meta charset="utf-8">
     <link href="https://fonts.googleapis.com/css?family=Ubuntu" rel="stylesheet">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="mobile-web-app-capable" content="yes">
@@ -11,12 +12,18 @@ function heading() {
     <meta name="description" content="Water levels on popular kayaking rivers around Scotland" />
     <meta name="og:description" content="Water levels on popular kayaking rivers around Scotland" />
     <script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
-<script type="text/javascript" src="http://canoescotland.org/sites/all/themes/basestation_open/js/riverLevels.js"></script>
-<script type="text/javascript" src="http://canoescotland.org/sites/all/themes/basestation_open/js/utilLib.js?"></script>
-<title>SCA Where's the Water</title>
-<style>
-body {font-family: 'Ubuntu', sans-serif;}
-</style>
+    <script type="text/javascript" src="http://canoescotland.org/sites/all/themes/basestation_open/js/riverLevels.js"></script>
+    <script type="text/javascript" src="http://canoescotland.org/sites/all/themes/basestation_open/js/utilLib.js?"></script>
+    <!-- Latest compiled and minified CSS -->
+    <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"> -->
+    <!-- jQuery library -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <!-- Latest compiled JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <title>SCA Where's the Water</title>
+    <style>
+        body {font-family: 'Ubuntu', sans-serif;}
+    </style>
 </head>
 
 <body style="background: white" onload="sortTable(0)">
