9ae67497ba6af3102cc45ad8f4e7f367c726ed86
Jonathan Riddell <jr@jriddell.org>
2018-09-15 18:25:25 +0100

    rejig the popup

--- lib/WheresTheWater.php
+++ lib/WheresTheWater.php
@@ -599,14 +599,30 @@ jQuery(document).ready( function(){
         $(document).ready( function(){
             $('#map').attr('style', 'position: relative; height: ' + $(window).height() + 'px');
             window.location.hash = '#map';
+            $('#map').on('click', '.js-info', function(){
+                    if (!$(this).hasClass('js-link')){
+                        $('.js-info-content').show();
+                        $('.js-chart-weekly-content').hide();
+                        $('.js-chart-monthly-content').hide();
+                        $('.js-chart-yearly-content').hide();
+                        $('.js-calib-table-content').hide();
+                        $(this).attr('style', '');
+                        $('.js-calib-table').attr('style', 'text-decoration: underline; color: blue; cursor: pointer');
+                        $('.js-chart-weekly').attr('style', 'text-decoration: underline; color: blue; cursor: pointer');
+                        $('.js-chart-monthly').attr('style', 'text-decoration: underline; color: blue; cursor: pointer');
+                        $('.js-chart-yearly').attr('style', 'text-decoration: underline; color: blue; cursor: pointer');
+                    }
+                }
+            );
             $('#map').on('click', '.js-calib-table', function(){
                     if (!$(this).hasClass('js-link')){
-                        $('.js-info').hide();
+                        $('.js-info-content').hide();
                         $('.js-chart-weekly-content').hide();
                         $('.js-chart-monthly-content').hide();
                         $('.js-chart-yearly-content').hide();
                         $('.js-calib-table-content').show();
                         $(this).attr('style', '');
+                        $('.js-info').attr('style', 'text-decoration: underline; color: blue; cursor: pointer');
                         $('.js-chart-weekly').attr('style', 'text-decoration: underline; color: blue; cursor: pointer');
                         $('.js-chart-monthly').attr('style', 'text-decoration: underline; color: blue; cursor: pointer');
                         $('.js-chart-yearly').attr('style', 'text-decoration: underline; color: blue; cursor: pointer');
@@ -615,12 +631,13 @@ jQuery(document).ready( function(){
             );
             $('#map').on('click', '.js-chart-weekly', function(){
                     if (!$(this).hasClass('js-link')){
-                        $('.js-info').hide();
+                        $('.js-info-content').hide();
                         $('.js-calib-table-content').hide();
                         $('.js-chart-weekly-content').show();
                         $('.js-chart-monthly-content').hide();
                         $('.js-chart-yearly-content').hide();
                         $(this).attr('style', '');
+                        $('.js-info').attr('style', 'text-decoration: underline; color: blue; cursor: pointer');
                         $('.js-calib-table').attr('style', 'text-decoration: underline; color: blue; cursor: pointer');
                         $('.js-chart-monthly').attr('style', 'text-decoration: underline; color: blue; cursor: pointer');
                         $('.js-chart-yearly').attr('style', 'text-decoration: underline; color: blue; cursor: pointer');
@@ -629,12 +646,13 @@ jQuery(document).ready( function(){
             );
             $('#map').on('click', '.js-chart-monthly', function(){
                     if (!$(this).hasClass('js-link')){
-                        $('.js-info').hide();
+                        $('.js-info-content').hide();
                         $('.js-calib-table-content').hide();
                         $('.js-chart-weekly-content').hide();
                         $('.js-chart-monthly-content').show();
                         $('.js-chart-yearly-content').hide();
                         $(this).attr('style', '');
+                        $('.js-info').attr('style', 'text-decoration: underline; color: blue; cursor: pointer');
                         $('.js-calib-table').attr('style', 'text-decoration: underline; color: blue; cursor: pointer');
                         $('.js-chart-weekly').attr('style', 'text-decoration: underline; color: blue; cursor: pointer');
                         $('.js-chart-yearly').attr('style', 'text-decoration: underline; color: blue; cursor: pointer');
@@ -643,12 +661,13 @@ jQuery(document).ready( function(){
             );
             $('#map').on('click', '.js-chart-yearly', function(){
                     if (!$(this).hasClass('js-link')){
-                        $('.js-info').hide();
+                        $('.js-info-content').hide();
                         $('.js-calib-table-content').hide();
                         $('.js-chart-weekly-content').hide();
                         $('.js-chart-monthly-content').hide();
                         $('.js-chart-yearly-content').show();
                         $(this).attr('style', '');
+                        $('.js-info').attr('style', 'text-decoration: underline; color: blue; cursor: pointer');
                         $('.js-calib-table').attr('style', 'text-decoration: underline; color: blue; cursor: pointer');
                         $('.js-chart-weekly').attr('style', 'text-decoration: underline; color: blue; cursor: pointer');
                         $('.js-chart-monthly').attr('style', 'text-decoration: underline; color: blue; cursor: pointer');
