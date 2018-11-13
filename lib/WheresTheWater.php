cc137ae7af887166cf3b457b78afef7cf9e48079
Jonathan Riddell <jr@jriddell.org>
2018-11-13 12:21:01 +0000

    spacing so controls do not get in way of text

--- lib/WheresTheWater.php
+++ lib/WheresTheWater.php
@@ -474,8 +474,8 @@ jQuery(document).ready( function(){
                 var riverReadingsTable = getRiverReadingsTable(riverSections[i], waterLevelValue);
                 var riverFilename = getRiverGraphFilename(riverSections[i]);
                 var icon = getWaterLevelIcon(riverSections[i]);
-                var contentString = "<div><h4 style='text-align: center'>" + riverSection + "</h4>" +
-                    "<p style='text-align: center'><img src='" + iconBase + waterLevelValue + ext + "' /> " +
+                var contentString = "<div><h4 style='padding-left: 30px;'>" + riverSection + "</h4>" +
+                    "<p style='padding-left: 30px;'><img src='" + iconBase + waterLevelValue + ext + "' /> " +
                     tidyStatusString(waterLevelValue) + ", " + currentReading + "</p>" +
                     "<p><span class='js-info'>Info</span> / <span class='js-calib-table link' style='text-decoration: underline; color: blue; cursor: pointer'>Calibrations</span> / <span class='js-chart-weekly link' style='text-decoration: underline; color: blue; cursor: pointer'>Weekly Chart</span> / <span class='js-chart-monthly link' style='text-decoration: underline; color: blue; cursor: pointer'>Monthly Chart</span> / <span class='js-chart-yearly link' style='text-decoration: underline; color: blue; cursor: pointer'>Yearly Chart</span></p>" +
                     "<p class='js-info-content'><img width='16' height='16' src='/wheres-the-water/pics/clock.png'/> Last reading " + currentReadingTime +
