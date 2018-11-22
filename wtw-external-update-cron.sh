a1b02c00e65c96ad03c9a20d06134a375e3f510d
Jonathan Riddell <jr@jriddell.org>
2018-11-22 14:33:57 +0000

    add cronjob script run on external server to trigger update

---
+++ wtw-external-update-cron.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#tell both servers to update river readings from SEPA
+wget http://`cat ~/bin/wtw-admin-ajfund`@www.andyjacksonfund.org.uk/wheres-the-water/admin/download-river-readings.php?download=1 -o /dev/null -O /dev/null
+wget http://`cat ~/bin/wtw-admin-canoescotland`@canoescotland.org/wheres-the-water/admin/download-river-readings.php?download=1  -o /home/jr/tmp/riverlog -O /home/jr/tmp/river
+
+# backup section data from SCA server into Git
+wget http://canoescotland.org/wheres-the-water/data/river-sections.json -o /dev/null -O /home/jr/tmp/river-sections.json
+if [ -s /home/jr/tmp/river-sections.json ]; then
+    python -mjson.tool /home/jr/tmp/river-sections.json > /home/jr/www/www.andyjacksonfund.org.uk/wheres-the-water/data/river-sections.json
+    cp /home/jr/www/www.andyjacksonfund.org.uk/wheres-the-water/data/river-sections.json /home/jr/www/www.andyjacksonfund.org.uk/wheres-the-water/data/river-sections-sca-copy.json 
+    cd /home/jr/www/www.andyjacksonfund.org.uk/wheres-the-water/data/
+    git diff
+    git add river-sections-sca-copy.json > /dev/null
+    git commit -m 'update river-sections-sca-copy.json from sca server' > /dev/null && git push 2>&1 > /dev/null
+else
+    echo "embra cron: downloaded river-sections.json has zero size, not updating git"
+fi
