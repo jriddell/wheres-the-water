2ce18b530255606b32e979e0ab7c17ac40f87a31
Jonathan Riddell <jr@jriddell.org>
2019-04-13 22:10:49 +0100

    debug

--- lib/GrabWeatherForecast.php
+++ lib/GrabWeatherForecast.php
@@ -60,6 +60,7 @@ class GrabWeatherForecast {
         foreach($this->weatherForecast['list'] as $forecast) {
             print "XXX foreach list\n";
             //show weather at 9 o'clock in morning and 3 in afternoon
+            print "XXX dt: " . $forecast['dt'] . "\n";
             if ($count < $max_forecasts and (date('G', $forecast['dt']) == "9" or date('G', $forecast['dt']) == "15")) {
                 print "XXX inside if\n";
                 $count = $count + 1;
