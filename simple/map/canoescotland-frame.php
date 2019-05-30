a321d3f717c88501aa4d1f4dc66d35d32badb1af
Jonathan Riddell <jr@jriddell.org>
2019-05-30 15:22:32 +0100

    add iframe for new canoescotland website

---
+++ simple/map/canoescotland-frame.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once '../../wheres-the-water/common.php';
+require_once '../../wheres-the-water/config.php';
+heading();
+require_once '../../wheres-the-water/lib/RiverSections.php';
+require_once '../../wheres-the-water/lib/WheresTheWater.php';
+
+$riverSections = new RiverSections;
+$riverSections->readFromJson();
+
+$wtw = new WheresTheWater;
+$wtw->theMap($riverSections);
+$wtw->theJavaScript();
+
+footer();
