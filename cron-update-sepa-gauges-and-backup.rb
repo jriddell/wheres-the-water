619ab499e53667a452388adafc0b046fdfc409a9
Jonathan Riddell <jr@jriddell.org>
2020-01-31 12:56:17 +0000

    remove debugging

--- cron-update-sepa-gauges-and-backup.rb
+++ cron-update-sepa-gauges-and-backup.rb
@@ -11,6 +11,7 @@ require 'net/http'
 require 'net/https'
 require 'json'
 require 'git'
+require 'logger'
 LOCK_FILE = Dir.pwd + '/' + 'CRON-UPDATE-SEPA-GAUGES-AND-BACKUP-LOCK'
 RIVER_SECTIONS_FILE = '/home/jr/www/www.andyjacksonfund.org.uk/wheres-the-water/data/river-sections.json'
 RIVER_SECTIONS_FILE_COPY = '/home/jr/www/www.andyjacksonfund.org.uk/wheres-the-water/data/river-sections-sca-copy.json'
@@ -20,18 +21,18 @@ class UpdateAndBackup
   def run_locked
     # Write lock file or quit if it exists
     if File.exist?(LOCK_FILE)
-        pp "Instance of cron-update-sepa-gauges-and-backup.rb already running"
+        #pp "Instance of cron-update-sepa-gauges-and-backup.rb already running"
         exit
     end
     lock_file = File.new(LOCK_FILE, 'w')
     lock_file.close
-    pp "lock written"
+    #pp "lock written"
 
     yield
 
     # Remove lock file
     File.delete(LOCK_FILE)
-    pp "lock deleted"
+    #pp "lock deleted"
   end
   
   def admin_login
@@ -54,10 +55,10 @@ class UpdateAndBackup
     response = Net::HTTP.get(uri2)
     #puts response
   end
-  
+
   def run
     run_locked do
-      pp "running locked"
+      #pp "running locked"
       trigger_sepa_gauges_update()
 
       # get JSON and tidy and backup
@@ -66,17 +67,12 @@ class UpdateAndBackup
       File.write(RIVER_SECTIONS_FILE_COPY, JSON.pretty_generate(river_sections_json))
       File.write(RIVER_SECTIONS_DEV_FILE, JSON.pretty_generate(river_sections_json))
 
-      # git diff and add and push
-      git = Git.open(Dir.pwd)
-      git.add('data/river-sections-sca-copy.json')
-      begin
-        git.commit('Update river-sections-sca-copy.json')
-        git.push
-      rescue StandardError => e
-        puts e.message
-      end
+      # commit to git
+      puts `git diff`
+      `git add data/river-sections-sca-copy.json`
+      `git commit -m 'update river-sections-sca-copy.json from embra server' && git push`
     end
-    pp "run done"
+    #pp "run done"
   end
 end
 
@@ -89,28 +85,3 @@ rescue StandardError => e
   pp "really deleting lock"
   File.delete(LOCK_FILE)
 end
-
-=begin
-
-
-
-#tell both servers to update river readings from SEPA
-wget https://`cat ~/bin/wtw-admin-ajfund`@www.andyjacksonfund.org.uk/wheres-the-water/admin/download-river-readings.php?download=1 -o /dev/null -O /dev/null
-wget https://`cat ~/bin/wtw-admin-ajfund`@dev.andyjacksonfund.org.uk/wheres-the-water/admin/download-river-readings.php?download=1 -o /dev/null -O /dev/null
-
-# backup section data from my server into Git
-python -mjson.tool /home/jr/www/www.andyjacksonfund.org.uk/wheres-the-water/data/river-sections.json > /home/jr/tmp/river-sections-tidy.json
-HEAD=`head -n1 /home/jr/tmp/river-sections-tidy.json`
-if [ $HEAD = '[' ]; then
-    cp /home/jr/tmp/river-sections-tidy.json /home/jr/www/dev.andyjacksonfund.org.uk/wheres-the-water/data/river-sections.json
-    cp /home/jr/tmp/river-sections-tidy.json /home/jr/www/www.andyjacksonfund.org.uk/wheres-the-water/data/river-sections.json
-    cp /home/jr/www/www.andyjacksonfund.org.uk/wheres-the-water/data/river-sections.json /home/jr/www/www.andyjacksonfund.org.uk/wheres-the-water/data/river-sections-sca-copy.json 
-    cd /home/jr/www/www.andyjacksonfund.org.uk/wheres-the-water/data/
-    git diff
-    git add river-sections-sca-copy.json > /dev/null
-    git commit -m 'update river-sections-sca-copy.json from embra server' > /dev/null && git push 2>&1 > /dev/null
-else
-    echo "embra cron: downloaded river-sections.json has zero size, not updating git"
-    cp /home/jr/www/www.andyjacksonfund.org.uk/wheres-the-water/data/river-sections-sca-copy.json /home/jr/www/www.andyjacksonfund.org.uk/wheres-the-water/data/river-sections.json
-fi
-=end
