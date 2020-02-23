7f3c8b44f20e57e3ab2358238e1dca2dd381cd4d
Jonathan Riddell <jr@jriddell.org>
2020-02-23 20:35:50 +0000

    description

--- common.php
+++ common.php
@@ -43,12 +43,12 @@ function heading($iframe=false) {
 function footer() {
 ?>
 <p>Newly added Feb 2020 Scheduled Water Flows.
+<img src="/wheres-the-water/pics/CONSTANT.png" width="10" height="10" />Constant. Flow is always on.
 <img src="/wheres-the-water/pics/TODAY.png" width="10" height="10" />Today, scheduled date on now.
 <img src="/wheres-the-water/pics/TOMORROW.png" width="10" height="10" />Tomorrow, scheduled date is mañana.
 <img src="/wheres-the-water/pics/NEXT_7_DAYS.png" width="10" height="10" />Next date is within 7 days.
 <img src="/wheres-the-water/pics/NEXT_30_DAYS.png" width="10" height="10" />Next date is within 30 days.
 <img src="/wheres-the-water/pics/NOT_THIS_MONTH.png" width="10" height="10" />Not This Month (schedule date over 30s days in future).
-<img src="/wheres-the-water/pics/CONSTANT.png.png" width="10" height="10" />Constant. Flow is always on.
 Dates are added manually.  <a href="https://jriddell.org/contact/">Feedback and contributions welcome</a>.</p>
 
 <p>SCA Where's the Water uses <a href="https://www2.sepa.org.uk/waterlevels/">water level data from SEPA</a>.</p>
