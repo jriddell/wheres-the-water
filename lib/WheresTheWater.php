8914a3a03c646e8951f783f5626c7460f5c97742
Jonathan Riddell <jr@jriddell.org>
2020-06-22 18:01:05 +0100

    update covid message

--- lib/WheresTheWater.php
+++ lib/WheresTheWater.php
@@ -617,7 +617,7 @@ jQuery(document).ready( function(){
                 var riverReadingsTable = getRiverReadingsTable(riverSections[i], waterLevelValue);
                 var riverFilename = getRiverGraphFilename(riverSections[i]);
                 var icon = getWaterLevelIcon(riverSections[i]);
-                var contentString = "<div><p>🦠 Be considerate, be conservative. Stay local, paddle within limits, meet only people from 1 other household a day but stay 2m apart. 😷</p><h4 style='padding-left: 30px;'>" + riverSection + "</h4>" +
+                var contentString = "<div><p>🦠 Be considerate, be conservative. Stay local, paddle within limits, groups of 3 households max and stay 2m apart. 😷</p><h4 style='padding-left: 30px;'>" + riverSection + "</h4>" +
                     "<p style='padding-left: 30px;'><img src='" + iconBase + waterLevelValue + ext + "' /> " +
                     tidyStatusString(waterLevelValue) + ", " + currentReading + "</p>" +
                     "<p><span class='js-info'>Info</span> / <span class='js-calib-table link' style='text-decoration: underline; color: blue; cursor: pointer'>Calibrations</span> / <span class='js-forecast link' style='text-decoration: underline; color: blue; cursor: pointer'>Weather</span>";
