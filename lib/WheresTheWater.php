65205500433d3d5537bc2454649cc5848c0605a6
Jonathan Riddell <jr@jriddell.org>
2020-07-01 17:09:00 +0100

    google maps link

--- lib/WheresTheWater.php
+++ lib/WheresTheWater.php
@@ -534,6 +534,7 @@ jQuery(document).ready( function(){
                             +riverSection['latitude']+"/"+riverSection['longitude']+"'>OpenStreetMap</a> / ";
             linksContent += "<span class='desktop'><a target='_blank' rel='noopener' href='https://www.bing.com/maps?cp="+riverSection['latitude']+"~"+riverSection['longitude']+"&lvl=14&style=s'>Ordnance Survey</a> /</span> ";
             linksContent += " <span class='desktop'><a target='_blank' rel='noopener' href='https://www.google.com/maps?q="+riverSection['latitude']+","+riverSection['longitude']+"'>Google Maps</a></span> ";
+            linksContent += " <span class='desktop'><a target='_blank' rel='noopener' href='https://maps.google.com/maps?z=12&t=h&q=loc:"+riverSection['latitude']+"+"+riverSection['longitude']+"'>Google Maps</a></span> ";
             linksContent += "<span class='mobile'><a href='geo:"+riverSection['latitude']+","+riverSection['longitude']+"'>Maps App</a><br /></span>";
 
             if ('guidebook_link' in riverSection && !riverSection['guidebook_link'].length == 0) {
