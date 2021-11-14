149cb4fc724ca3dd8d235d521844f331323038cf
Jonathan Riddell <jr@jriddell.org>
2021-11-14 20:17:43 +0000

    show debug

--- admin/download-river-readings.php
+++ admin/download-river-readings.php
@@ -1,7 +1,12 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 require_once('../config.php');
 require_once('../lib/RiverSections.php');
 require_once('../lib/GrabSepaRivers.php');
+
 
 ?>
 
