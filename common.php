d6cfaa0742741de8eee8affc34f7b48f9a54fc60
Jonathan Riddell <jr@jriddell.org>
2024-12-29 21:15:35 +0000

    debrand

--- common.php
+++ common.php
@@ -20,7 +20,7 @@ function heading($iframe=false) {
     <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script> -->
     <!-- Latest compiled JavaScript -->
     <!-- <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script> -->
-    <title>Paddle Scotland Where's the Water</title>
+    <title><!-- Paddle Scotland--> Where's the Water</title>
     <style>
         body {font-family: 'Ubuntu', sans-serif;}
     </style>
@@ -31,9 +31,9 @@ function heading($iframe=false) {
     if (!$iframe) {
 ?>
 <p><a href="http://www.andyjacksonfund.org.uk"><img src="/wheres-the-water/andy-jackson-fund.png" width="350" /></a>
-<a href="https://www.paddlescotland.org.uk"><img src="/wheres-the-water/pics/paddle-scotland.png" width="350" /></a></p>
+<!-- <a href="https://www.paddlescotland.org.uk"><img src="/wheres-the-water/pics/paddle-scotland.png" width="350" /></a>--></p>
 
-<h1>Paddle Scotland Where&#039;s The Water?</h1>
+<h1><!-- Paddle Scotland--> Where&#039;s The Water?</h1>
 
 <h2>Scottish River Levels</h2>
 <?php
@@ -56,7 +56,7 @@ function footer() {
 <img src="/wheres-the-water/pics/NO_KNOWN_DATES.png" width="10" height="10" /> No known dates.<br>
 Dates are added manually.   In case of discrepancy the info page linked is definitive over this list.   <a href="https://jriddell.org/contact/">Feedback and contributions welcome</a>.</p>
 
-<p>Paddle Scotland Where's the Water uses <a href="https://www2.sepa.org.uk/waterlevels/">water level data from SEPA</a>.</p>
+<p><!-- Paddle Scotland--> Where's the Water uses <a href="https://www2.sepa.org.uk/waterlevels/">water level data from SEPA</a>.</p>
 
 <p>Code written and maintained by <a href="http://www.edinburghlinux.co.uk">Jonathan Riddell</a>. <a href="https://github.com/jriddell/wheres-the-water">Code on GitHub</a>.  Please file <a href="https://github.com/jriddell/wheres-the-water/issues">bug reports and feature requests on GitHub</a>.</p>
 
@@ -65,7 +65,7 @@ Dates are added manually.   In case of discrepancy the info page linked is defin
 <p><a href="http://goo.gl/forms/YQ3xZTi30vrtFYpo1">River Gauge Request Form</a></a>.</p>
 <p><a href="/wtw">Simple List View</a></p>
 <p><a href="/wtw/map">Simple Map View</a></p>
-<p><a href="https://www.paddlescotland.org.uk/where-go/wheres-water">Full Map View</a></p>
+<!-- <p><a href="https://www.paddlescotland.org.uk/where-go/wheres-water">Full Map View</a></p> -->
 </body>
 </html>
 <?php
