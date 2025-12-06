5e5221eb8a815c07c04e8afc4000a90ddcdfd6ce
Jonathan Riddell <jr@jriddell.org>
2025-12-06 20:13:35 +0000

    convert test to Timeseries

--- tests/GrabSepaGaugesTest.php
+++ tests/GrabSepaGaugesTest.php
@@ -1,50 +1,49 @@
 <?php
 
 require_once('../config.php');
-require_once('../lib/GrabSepaGauges.php');
-
+require_once('../lib/GrabSepaGaugesTimeseries.php');
 use PHPUnit\Framework\TestCase;
 
 final class GrabSepaGaugesTest extends TestCase
 {
 
     public function testVerifyCsvData() {
-        $grabSepa = new GrabSepaGauges();
-        $grabSepa->sepaCsvData = file_get_contents("data/SEPA_River_Levels_Web-good.csv");
-        $this->assertEquals(true, $grabSepa->verifyCsvData());
-        $grabSepa->sepaCsvData = file_get_contents("data/SEPA_River_Levels_Web-bad2.csv");
-        $this->assertEquals(false, $grabSepa->verifyCsvData());
-        $grabSepa->sepaCsvData = file_get_contents("data/SEPA_River_Levels_Web-bad.csv");
-        $this->assertEquals(false, $grabSepa->verifyCsvData());
+        $grabSepa = new GrabSepaGaugesTimeseries();
+        $grabSepa->sepaJsonData = file_get_contents("data/getStationList-good.json");
+        $this->assertEquals(true, $grabSepa->verifyJsonData());
+        $grabSepa->sepaJsonData = file_get_contents("data/getStationList-bad.json");
+        $this->assertEquals(false, $grabSepa->verifyJsonData());
+        $grabSepa->sepaJsonData = file_get_contents("data/getStationList-bad2.json");
+        $this->assertEquals(false, $grabSepa->verifyJsonData());
     }
 
     public function testDoGrab()
     {
-        $grabSepa = new GrabSepaGauges();
+        $grabSepa = new GrabSepaGaugesTimeseries();
         $grabSepa->doGrab();
         $this->assertEquals(
-            'data/SEPA_River_Levels_Web.csv',
+            'data/getStationList-good.json',
             $grabSepa->sepaFile
         );
         unlink($grabSepa->sepaFile);
-        $grabSepa2 = new GrabSepaGauges();
+        $grabSepa2 = new GrabSepaGaugesTimeseries();
         $grabSepa2->doGrab();
         $this->assertEquals(
-            'data/SEPA_River_Levels_Web.csv',
+            'data/SgetStationList-good.json',
             $grabSepa->sepaFile
         );
         
     }
     
-    public function testConvertCsvToArray() {
-        $grabSepa = new GrabSepaGauges();
-        $grabSepa->sepaCsvData = file_get_contents("data/SEPA_River_Levels_Web-good.csv");
-        $grabSepa->convertCsvToArray();
-        $this->assertEquals(['reading_timestamp'=> 1515845700, 'gauge_name' => 'Perth'], $grabSepa->sepaData[10048]);
+    public function testConvertJsonToArray() {
+        $grabSepa = new GrabSepaGaugesTimeseries();
+        $grabSepa->sepaCsvData = file_get_contents("data/getStationList-good.json");
+        $grabSepa->convertJsonToArray();
+        $this->assertEquals(['gauge_name' => 'Perth'], $grabSepa->sepaData[10048]);
     }
     
     public function testSepaData() {
-        $grabSepa = new GrabSepaGauges();
+        $grabSepa = new GrabSepaGaugesTimeseries();
         $sepaData = $grabSepa->sepaData();
         $this->assertGreaterThan(20, sizeof($sepaData));
         $this->assertInternalType('array', $sepaData);
